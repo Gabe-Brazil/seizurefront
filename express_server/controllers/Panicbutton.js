48bddba662fff2fdd387ae0324660afa57361f44
Gabe-Brazil <122241972+Gabe-Brazil@users.noreply.github.com>
2025-02-13 21:23:58 -0500

    server side deleted

--- express_server/controllers/Panicbutton.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const router=express.Router()
-//const Model=require("../models/Panicbuttonmodel.js")
-
-
-//router.get("/",Model.getPanicconfigure)
-//router.put("/",Model.updatePanicconfigure)
-
-module.exports=router
--- express_server/replit.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs }: {
-    deps = [
-        pkgs.sqlite.bin
-        pkgs.nodejs-16_x
-        pkgs.cowsay
-    ];
-}
